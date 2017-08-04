32c82fdeec982c8e5a57fdc7832437b263cf3bc6
djyangguang <yangg.1985@163.com>
2017-08-04 15:58:06 +0800

    2017年7月28日17:15:14

---
+++ src/com/spark/study/spark/java/Streaming/MySQLWC.java
@@ -0,0 +1,100 @@
+package com.spark.study.spark.java.Streaming;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import com.google.common.base.Optional;
+
+import scala.Tuple2;
+/**
+ * Created by yg on 2017/8/4.
+ */
+public class MySQLWC {
+    public static void main (String[] args){
+        SparkConf conf = new SparkConf().setAppName("d").setMaster("local[2]");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
+        jssc.checkpoint("hdfs://node1:9000/wc_checkpoint");
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("node1",9999);
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterable<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(" "));
+            }
+        });
+
+        JavaPairDStream<String,Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<String,Integer>(s,1);
+            }
+        });
+
+        JavaPairDStream<String, Integer> wordcounts = pairs.updateStateByKey(
+
+                new Function2<List<Integer>, Optional<Integer>, Optional<Integer>>(){
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Optional<Integer> call(List<Integer> values,
+                                                  Optional<Integer> state) throws Exception {
+                        Integer newValue = 0;
+                        if(state.isPresent()){
+                            newValue = state.get();
+                        }
+                        for(Integer value : values){
+                            newValue += value;
+                        }
+                        return Optional.of(newValue);
+                    }
+                });
+        //持久化
+        // 每次得到当前所有单词的统计计数之后,讲其持久化以便于后续J2EE程序显示
+        // 每次得到当前所有单词的统计计数之后,讲其持久化以便于后续J2EE程序显示
+       /*   wordcounts.foreachRDD(new VoidFunction<JavaPairRDD<String,Integer>>(){
+
+            @Override
+            public void call(JavaPairRDD<String, Integer> wordcountsRDD)
+                  throws Exception {
+                wordcountsRDD.foreachPartition(new VoidFunction<Iterator<Tuple2<String,Integer>>>(){
+
+                   @Override
+                    public void call(Iterator<Tuple2<String, Integer>> wordcounts)
+                            throws Exception {
+                        Connection conn = ConnectionPool.getConnection();
+
+                        Tuple2<String,Integer> wordcount = null;
+                        while(wordcounts.hasNext()){
+                            wordcount = wordcounts.next();
+                            String sql = "insert into wordcount(word,count) "
+                                    + "values('" + wordcount._1 + "'," + wordcount._2 + ")";
+                            Statement stmt = conn.createStatement();
+                            stmt.executeUpdate(sql);
+                        }
+
+                        ConnectionPool.returnConnection(conn);
+                    }
+                });
+            }
+
+        });*/
+        jssc.start();
+        jssc.awaitTermination();
+        jssc.close();
+    }
+}
